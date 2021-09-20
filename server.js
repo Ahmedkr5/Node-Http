8eeeba0a7efe8372ffd4d48562b27ca93393543c
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-09-20 13:53:51 +0100

    returning html files from a folder+method testing with postman

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const http=require ('http');
-
+const fs = require('fs');
+const path = require('path');
 
 var server =http.createServer((req,res)=>{
-    res.writeHead(200,{"Content-Type":"text/html"});
-    res.end('<html><body><h1>ceci est ma reponse du serveur!</h1></body></html>');
-}
+ //   res.writeHead(200,{"Content-Type":"text/html"});
+ //   res.end("ceci est ma reponse du serveur");
+ console.log('Request for ' + req.url + ' by method ' + req.method);
 
-);
+ if (req.method == 'GET') {
+   var fileUrl;
+   if (req.url == '/') fileUrl = '/index.html';
+   else fileUrl = req.url;
+
+   var filePath = path.resolve('./public'+fileUrl);
+   const fileExt = path.extname(filePath);
+   if (fileExt == '.html') {
+     fs.exists(filePath, (exists) => {
+       if (!exists) {
+         res.statusCode = 404;
+         res.setHeader('Content-Type', 'text/html');
+         res.end('<html><body><h1>Error 404: ' + fileUrl + 
+                     ' not found</h1></body></html>');
+         return;
+       }
+       res.statusCode = 200;
+       res.setHeader('Content-Type', 'text/html');
+       fs.createReadStream(filePath).pipe(res);
+     });
+   }
+   else {
+     res.statusCode = 404;
+     res.setHeader('Content-Type', 'text/html');
+     res.end('<html><body><h1>Error 404: ' + fileUrl + 
+             ' not a HTML file</h1></body></html>');
+   }
+ }
+ else {
+     res.statusCode = 404;
+     res.setHeader('Content-Type', 'text/html');
+     res.end('<html><body><h1>Error 404: ' + req.method + 
+             ' not supported</h1></body></html>');
+ }
+});
 
 server.listen(8080);
